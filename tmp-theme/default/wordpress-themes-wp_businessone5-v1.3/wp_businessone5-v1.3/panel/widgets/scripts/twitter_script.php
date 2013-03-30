9c237e4a55df25b25a9007fce3a9dc0001c8915b
Palmieri Andrioli Hell <delpalmis@hotmail.com>
2013-03-30 19:41:55 -0300

    Utilizando um novo tema WordPress.
    O tema está sendo adaptado para servidores de l2j.

--- tmp-theme/default/wordpress-themes-wp_businessone5-v1.3/wp_businessone5-v1.3/panel/widgets/scripts/twitter_script.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$functionname = preg_replace('/[^a-zA-Z0-9]/','', urldecode($_GET['func']));
-	$widget_id = preg_replace('/[^a-zA-Z0-9]/','', urldecode($_GET['widget_id']));
-	
-	// The rest of the file is supposed to be javascript.
-?>
-	function <?php echo $functionname; ?>(twitters){ 
-		twitterCallback2(twitters, "<?php echo $widget_id;?>"); 
-	}
