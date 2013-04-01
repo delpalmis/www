1bc859e2d67a9cc6a7167585111df7ed2eb24057
Palmieri Andrioli Hell <delpalmis@hotmail.com>
2013-04-01 18:53:21 -0300

    Atualizações no estilos e no Template

--- tmp-content/tmp-includes/tmp-functions.php
+++ tmp-content/tmp-includes/tmp-functions.php
@@ -1,8 +1,11 @@
 <?php
 function tmp_not_open($page){
-	if(eregi($page, $_SERVER['SCRIPT_NAME'])){
+	if(eregi($_SERVER['PHP_SELF'])){
 		die("<script>window.alert('Você não tem permissão para acessar essa página!'</script>");
 	}
 }
-tmp_not_open("tmp_functions.php");
+function tmp_get_pages($pagina){
+	//empty
+}
+tmp_not_open("tmp-functions.php");
 ?>
